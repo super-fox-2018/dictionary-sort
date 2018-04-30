80ff795b638df4514bc0c1e15cc8cb5b0e8b0533
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-05-01 00:56:47 +0700

    Dictionary Sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,19 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+
+function Dictionary(arrOfWord) {
+    for (var i=0; i<arrOfWord.length; i++){
+        for (var j=i+1; j<arrOfWord.length; j++){
+            if (arrOfWord[i] >= arrOfWord[j]){
+                var temp=arrOfWord[i];
+                arrOfWord[i]=arrOfWord[j];
+                arrOfWord[j]=temp;
+              }
+        }
+    }
+    return arrOfWord.join()
+}
+
+console.log(Dictionary(["makan", "duduk", "tidur", "terbang"])); // duduk,makan,terbang,tidur
+console.log(Dictionary(["anggi", "angga", "ani", "adi"])); // adi,angga,anggi,ani
 
 module.exports = Dictionary
