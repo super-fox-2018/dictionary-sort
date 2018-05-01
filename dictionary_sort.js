44758a36b627d2e0e6c4117e4dd5573f4ba76aba
ridozaen <ridozaen@gmail.com>
2018-05-01 15:58:24 +0700

    added code into dictionary_sort.js

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,30 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function dictionarySort(arrDict) {
+    for (var i = 0; i < arrDict.length; i++) {
+        // console.log(arrDict[i].toLowerCase().charCodeAt(0), arrDict[i][0], arrDict[i+1][0]);
+        // console.log(arrDict[i][0].toLowerCase() === arrDict[i+1][0].toLowerCase());
+        let tempSwap = '';
+        let minInd = 0;
+        for (var j = i; j < arrDict.length; j++) {
+            if (i !== j) {
+                if (tempSwap.toLowerCase() > arrDict[j].toLowerCase()) {
+                    tempSwap = arrDict[j];
+                    minInd = [j];
+                }
+            } else {
+                tempSwap = arrDict[i];
+                minInd = i;
+            }
+        }
+        arrDict[minInd] = arrDict[i];
+        arrDict[i] = tempSwap;
+    }
+    return arrDict.join(',');
+}
 
-module.exports = Dictionary
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
+console.log(dictionarySort(['vnggi', 'Dngga', 'wni', 'qdi']));
+
+module.exports = dictionarySort
