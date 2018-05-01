9770d8053e01673a75d1ea59aceb9c95c915c992
Michael Cangcianno <michaelcangcianno@Michaels-MacBook-Pro.local>
2018-05-01 16:36:04 +0700

    finished dictionary sort problem

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,27 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function sorting(array) {
+  let min = 0;
+  let minIndex = 0;
+  for (var i = 0; i < array.length; i++) {
+    min = array[i];
+    minIndex = i;
+    for (var j = i; j < array.length; j++) {
+      if (array[j].toLowerCase() < min.toLowerCase()) {
+        min = array[j];
+        minIndex = j;
+      }
+    }
+    array[minIndex] = array[i];
+    array[i] = min;
+  }
+  return array.join(',');
+}
+
+let arr1 = ['makan', 'duduk', 'Tidur', 'terbang'];
+let arr2 = ['anggi', 'Angga', 'adi', 'ani'];
+
+console.log(sorting(arr1));
+console.log(sorting(arr2));
+
+// module.exports = Dictionary;
