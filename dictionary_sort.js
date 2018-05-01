1346feb7c080af9eb4e0159d896dc9bae135ef30
justinusamwijaya <justin.am.wijaya@gmail.com>
2018-05-01 14:29:08 +0700

    jawaban dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,22 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+var DictSort = (x)=>{
+    for(i=1;i<x.length;i++){
+        if(x[i-1].toLowerCase()>x[i].toLowerCase()){
+            temp = x[i-1]
+            x[i-1] = x[i]
+            x[i] = temp
+            i=0
+        }
+    }
+    return x
 
-module.exports = Dictionary
+}
+
+
+
+
+
+
+
+
+console.log(DictSort(['Makan','duduk','tidur','terbang'])) //duduk,Makan,terbang,tidur
+console.log(DictSort(['anggi','angga','ani','adi']))//adi, angga, anggi, ani
