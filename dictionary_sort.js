23c26945fa3ea0c15ed5a1ff934242824daa2a44
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-05-01 09:05:40 +0700

    brama-prasetyo

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,18 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
-
-module.exports = Dictionary
+// const readline = require('readline');
+//
+// module.exports = Dictionary
+function dictionarySort(data) {
+  for (var i = 0; i < data.length; i++) {
+    var firstDataI = ''
+    for (var j = 0; j < data.length; j++) {
+      if (data[i] < data[j]) {
+        firstDataI = data[i]
+        data[i] = data[j]
+        data[j] = firstDataI
+      }
+    }
+  }
+  return data;
+}
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
