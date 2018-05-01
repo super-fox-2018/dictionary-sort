cfb8fa1d30ec2cf8bb476078d37423f2338473d0
Rin Ng <Rin08.ng@gmail.com>
2018-05-01 23:04:10 +0700

    added code for dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,45 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
+function arrOfWords(arr){
+  var newArr = [];
+  var temp = arr[0];
+  var position = 0;
+
+if(arr.length === 0){
+  return "";
+}
+else{
+ for(var i = 0; i < arr.length; i++){
+   if(temp.toLowerCase() > arr[i].toLowerCase()){
+     temp = arr[i];
+     position = i;
+   }
+ }
+    newArr.push(temp);
+    arr.splice(position, 1);
+
+    if(arr.length >= 1){
+    return (newArr + ", " + arrOfWords(arr));
+    }
+    else{
+      return(newArr + " "  + arrOfWords(arr));
+    }
+}
+
+}
+
+
+
+
+
+
+console.log(arrOfWords(["makan","duduk", "tidur", "terbang"]));
+
+console.log(arrOfWords(["anggi","angga", "adi", "ani"]));
+
+console.log(arrOfWords(["tracer","d.va", "sombra", "moira"]));
+
+console.log(arrOfWords(["ganteng","genting", "gunting", "genteng"]));
+
 module.exports = Dictionary
