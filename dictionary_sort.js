fe4e9005370759b37af88ffaaa00473c24ce757e
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-01 16:31:53 +0700

    function dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
+'use strict'
+
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+var arrOfWord2 = ['anggi', 'angga', 'ani', 'adi'];
+
+function dictSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(dictSort(arrOfWord))
+console.log(dictSort(arrOfWord2));
+
+// module.exports = Dictionary
