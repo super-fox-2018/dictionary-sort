af4c1a781178e6d9158b7e634f3eae42a21be7e4
adrowicaksono <adrowicaksono@gmail.com>
2018-05-01 19:13:15 +0700

    finish exercise dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,21 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function dictionarySort(arr){
+    for(let a=0; a<=arr.length-1; a++){
+        for(let b=a+1; b<=arr.length-1; b++){
+            var temp = arr[a];
+            if(arr[a]>=arr[b]){
+                arr[a] = arr[b];
+                arr[b] = temp;
+            }
+        }
+    }
+    console.log(arr);
+}
 
-module.exports = Dictionary
+
+
+var arrOfWord = ['makan','duduk','tidur','terbang'];
+var arrOfWord2 = ['anggi','angga','ani','adi'];
+dictionarySort(arrOfWord2)
+//module.exports = Dictionary
