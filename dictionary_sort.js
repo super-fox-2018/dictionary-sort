7739c75a5a64a56a89c1a167c0553bec1755dc0f
unknown <chuddywarrior@gmail.com>
2018-05-06 02:28:44 +0700

    changes for dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,22 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function sorting(array) {
+for(var z = 0; z < array.length; z++){
+  for(var j = z; j < array.length; j++){
+    if(array[j].toLowerCase() < array[z].toLowerCase()){
+      let swap = array[z];
+      array[z] = array[j];
+      array[j] = swap;
+    }
+  }return array;
+}
+
+}
+let arr1 = ['makan', 'duduk', 'Tidur', 'terbang'];
+let arr2 = ['anggi', 'Angga', 'adi', 'ani'];
+
+
+console.log(sorting(arr1));
+console.log(sorting(arr2));
+
+// module.exports = Dictionary
